76813171606b820e1e826f150948691ba51ed428
Fletcher Heisler <fheisler@users.noreply.github.com>
2018-04-30 19:33:47 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
+PORT = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
